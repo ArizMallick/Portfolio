ce7cf7a1229bfcfdd500de496002fa3cb5727cda
Md Ariz Hasan <61147881+ArizMallick@users.noreply.github.com>
2026-02-12 22:08:42 +0530

    Global Hosting

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ export default {
   server: {
     host: "0.0.0.0",
     port: 5173
-  }
+  },
   base: '/arizhasan.tech/',
   build: {
     chunkSizeWarningLimit: 1000, // Adjust the limit to 1000 kB
