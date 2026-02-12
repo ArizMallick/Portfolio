bdae8e266648880dbcac4d3d105326175fb7292c
Md Ariz Hasan <61147881+ArizMallick@users.noreply.github.com>
2026-02-12 22:02:07 +0530

    Global Hosting

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 export default {
-  base: '/3d-portfolio/',
+  server: {
+    host: "0.0.0.0",
+    port: 5173
+  }
+  base: '/arizhasan.tech/',
   build: {
     chunkSizeWarningLimit: 1000, // Adjust the limit to 1000 kB
   },
